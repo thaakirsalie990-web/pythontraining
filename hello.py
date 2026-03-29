b0eab031f37100fd8e17ba193a81baf90816f49e
Thaakir <thaakirsalie990@gmail.com>
2026-03-29 20:06:16 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,52 @@
+# This is like the console.log() function in JS
+print("This is my first python code") 
+
+# Basic operators:
+
+# Addition
+print(5 + 5)
+# Subtraction
+print(10 - 6)
+# Division
+print(6 / 3)
+# Multiplication
+print(6 * 5)
+
+# Advance Mathematical Operators
+
+# Exponentials: the double ** serves as the operator and the number after it is the power of it
+print(4 ** 2)
+print(4 ** 3)
+
+# Modulus Division
+print(3 // 4)
+
+# Percentage
+print(6 % 50)
+
+# ==========================================
+# Checking Data Types
+
+# int/ integers = used for counting whole numbers or numbers without decimal points
+print(type(10))
+
+# Floats = used for numbers with decimal points. // What is the best use case for this in terms of real world projects
+print(type(3.14))
+
+# Complex = used for advance mathematics == real part + imaginary part // How do these work? What use cases are there.
+print(type(1 + 2j))
+
+# Stings/ str = I think an explanation is needed. it is the same like JS
+print(type("Tommy"))
+
+# List = same like JS
+print(type([1, 2, 23]))
+
+# Dictionaries = seems like arrays
+print(type({'name': 'Anebeth'}))
+
+# Sets = a list?
+print(type({1,2, 2, 3}))
+
+# Tupple = can't be changed like const variable in JS
+print(type((1,2,3,4)))
